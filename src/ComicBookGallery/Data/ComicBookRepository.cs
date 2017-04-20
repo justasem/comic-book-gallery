16c75163105521d75f8626cb236dd86ad924639e
justasem <justasem@gmail.com>
2017-04-20 10:06:23 +0300

    updated ComicBooksController to add listview

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -59,6 +59,11 @@ namespace ComicBookGallery.Data
             }
         };
 
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicbook(int id)
         {
             ComicBook comicBookToReturn = null;
